8e72cccd3f8318676a56dc72d7c1cd017b650fd6
Sergey Kasitsin <kasitsinsergey@gmail.com>
2022-06-12 21:42:43 +0300

    Task1 Completed

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//1. Показать натуральные числа от M до N, N и M заданы
+
+Console.Write("Введите первое натуральное число: ");
+int M = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе натуральное число: ");
+int N = int.Parse(Console.ReadLine());
+
+if (M < N)
+{
+    for (int i = M; i <= N; i++)
+    {
+        Console.WriteLine($"{i}");
+    }
+}
+else
+{
+    for (int i = M; i >= N; i--)
+    {
+        Console.WriteLine($"{i}");
+    }
+}
